a205b9ca6b38c787ce549032057c0a181c4e4ad5
Andrianarivo David <andrianarivo.david@gmail.com>
2023-05-30 22:35:46 +0300

    build calcultor ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Calculator from './components/Calculator';
 
 function App() {
   return (
     <div className="App">
-      Hello world
+      <Calculator />
     </div>
   );
 }
